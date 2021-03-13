96947f465ff07ddebb005f4e31f3b37c4c70a9ef
Sergey <szaytsev060576@gmail.com>
2021-03-13 10:38:59 +0300

    правки

--- js/filtration.js
+++ js/filtration.js
@@ -13,142 +13,65 @@ const filterHousing = formFiltration.querySelector('#housing-type');
 const filterPrice = formFiltration.querySelector('#housing-price');
 const filterRooms = formFiltration.querySelector('#housing-rooms');
 const filterGuests = formFiltration.querySelector('#housing-guests');
-// const listFilterFeatures = formFiltration.querySelectorAll('.map__checkbox');
+const listFilterFeatures = formFiltration.querySelectorAll('.map__checkbox');
 
 
-// const  getCheckedCheckBoxes = (list) => {
-//   const checkboxesChecked = [];
-//   for (let index = 0; index < list.length; index++) {
-//     if (list[index].checked) {
-//       checkboxesChecked.push(list[index].value);
-//     }
-//   }
-//   console.log(checkboxesChecked)
-//   return checkboxesChecked;
-// }
-// getCheckedCheckBoxes(listFilterFeatures);
-
-const setFilterHousingChange = (cb) => {
-  filterHousing.addEventListener('change', () => {
+const setFilterFormChange = (cb) => {
+  formFiltration.addEventListener('change', () => {
     removeSimilarAdMarkers(markersSimilarAd);
     cb();
   });
 };
 
-const setFilterRoomsChange = (cb) => {
-  filterRooms.addEventListener('change', () => {
-    removeSimilarAdMarkers(markersSimilarAd);
-    cb();
-  });
-};
-
-const setFilterGuestsChange = (cb) => {
-  filterGuests.addEventListener('change', () => {
-    removeSimilarAdMarkers(markersSimilarAd);
-    cb();
-  });
-};
-
-const setFilterPriceChange = (cb) => {
-  filterPrice.addEventListener('change', () => {
-    removeSimilarAdMarkers(markersSimilarAd);
-    cb();
-  });
-};
-
-// const setFilterFeaturesChange = (cb) => {
-//   for(const element of listFilterFeatures) {
-//     element.addEventListener('change', () => {
-//       removeSimilarAdMarkers(markersSimilarAd);
-//       console.log('checked')
-//       cb();
-//     });
-//   }
-// };
-
-
-//для примера
-// const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
-// const result = words.filter(word =>{
-//   console.log(word.length > 6)
-// } );
-// console.log(result)
-
-//Фильтр по типу жилья
-
-const filterAdvertisementsOfType = (element) => {
+const isTypeOfHouse = (element) => {
   const {offer} = element;
-  if (filterHousing.value === offer.type){
-    return element;
-  }
-  if (filterHousing.value === DefaultValue.ANY_VALUE){
-    return element;
-  }
+  return  filterHousing.value === offer.type ||
+  filterHousing.value === DefaultValue.ANY_VALUE;
 };
-//Фильтр по кол-ву комнат
 
-const filterAdvertisementsOfRooms = (element) => {
+const isRoomsInAd = (element) => {
+  const {offer} = element;
+  return parseInt(filterRooms.value) === offer.rooms ||
+  filterRooms.value === DefaultValue.ANY_VALUE;
+};
+
+const isGuestsInAd = (element) => {
+  const {offer} = element;
+  return parseInt(filterGuests.value) === offer.guests ||
+  filterGuests.value === DefaultValue.ANY_VALUE;
+};
+
+const isPriceOfHouse = (element) => {
   const {offer} = element
-  if (parseInt(filterRooms.value) === offer.rooms){
-    return element;
-  }
-  if (filterRooms.value === DefaultValue.ANY_VALUE){
-    return element;
-  }
+  return filterPrice.value === filterPrice[0].value ||
+  filterPrice.value === filterPrice[1].value && offer.price > DefaultValue.MIDDLE_MIN && offer.price <= DefaultValue.MIDDLE_MAX ||
+  filterPrice.value === filterPrice[2].value && offer.price <= DefaultValue.LOW ||
+  filterPrice.value === filterPrice[3].value && offer.price >= DefaultValue.HIGH;
 };
 
-//Фильтр по количеству гостей
-
-const filterAdvertisementsOfGuests = (element) => {
-  const {offer} = element
-  if (parseInt(filterGuests.value) === offer.guests){
-    return element;
+const isCheckedFeatures = () => {
+  const checkboxesChecked= [];
+  for (let element of  listFilterFeatures){
+    if (element.checked){
+      checkboxesChecked.push(element.value);
+    }
   }
-  if (filterGuests.value === DefaultValue.ANY_VALUE){
-    return element;
-  }
+  return checkboxesChecked;
 };
 
-//Фильтр по цене
-
-const filterAdvertisementsOfPrice = (element) => {
-  const {offer} = element
-  if(filterPrice.value === filterPrice[0].value){
-    return element;
-  }
-  if(filterPrice.value === filterPrice[1].value && offer.price > DefaultValue.MIDDLE_MIN && offer.price <= DefaultValue.MIDDLE_MAX){
-    return element;
-  }
-  if(filterPrice.value === filterPrice[2].value && offer.price <= DefaultValue.LOW){
-    return element;
-  }
-  if(filterPrice.value === filterPrice[3].value && offer.price >= DefaultValue.HIGH){
-    return element;
-  }
+const  isFeatureInAd = (element) => {
+  const {offer} = element;
+  const checkedList = isCheckedFeatures()
+  checkedList.map(el => el.value);
+  return checkedList.every((feature) => offer.features.includes(feature));
 };
 
+const filterAdvertisements = (data) => {
+  return isTypeOfHouse(data) &&
+  isRoomsInAd(data) &&
+  isGuestsInAd(data) &&
+  isPriceOfHouse(data)
+  && isFeatureInAd(data);
+};
 
-// Фильтр удобств
-
-// const filterAdvertisementsOfFeatures = (element) => {
-//   const {offer} = element;
-
-
-//   return offer.features.filter()
-
-// };
-
-// console.log()
-//   features: ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"]
-
-
-// const filterAdvertisements = () => {
-//   filterAdvertisementsOfType
-//   filterAdvertisementsOfPrice
-//   filterAdvertisementsOfRooms
-//   filterAdvertisementsOfGuests
-// };
-
-
-export{setFilterHousingChange, setFilterPriceChange, setFilterRoomsChange, setFilterGuestsChange/* , setFilterFeaturesChange */}
-export{filterAdvertisementsOfType, filterAdvertisementsOfPrice, filterAdvertisementsOfRooms, filterAdvertisementsOfGuests/* , filterAdvertisementsOfFeatures */}
+export {setFilterFormChange, filterAdvertisements};
