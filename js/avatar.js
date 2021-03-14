b00e7c0a2eee3b51135a6fdfcbc5afe873945198
Sergey <szaytsev060576@gmail.com>
2021-03-14 18:43:27 +0300

    сброс превью фото

--- js/avatar.js
+++ js/avatar.js
@@ -2,15 +2,19 @@ const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
 const avatarChooser = document.querySelector('#avatar');
 const previewAvatar = document.querySelector('.ad-form-header__preview > img');
+const previewFile = document.createElement('img');
 const fileChooser = document.querySelector('#images');
 const blockFile = document.querySelector('.ad-form__photo');
-const previewFile = document.createElement('img');
-blockFile.appendChild(previewFile);
-previewFile.setAttribute('src', '');
-previewFile.setAttribute('width', '100%');
-previewFile.setAttribute('height', '100%');
-previewFile.style.objectFit = 'cover';
 
+
+const getPreviewImage = () => {
+  blockFile.appendChild(previewFile);
+  previewFile.setAttribute('src', '');
+  previewFile.setAttribute('width', '100%');
+  previewFile.setAttribute('height', '100%');
+  previewFile.style.objectFit = 'cover';
+  return previewFile;
+};
 
 const setImage = (el, preview) => {
   const file = el.files[0];
@@ -31,10 +35,21 @@ const setImage = (el, preview) => {
   }
 };
 
+
+const removeAvatar = () => {
+  previewAvatar.src = 'img/muffin-grey.svg';
+}
+
+const removeImage = () => {
+  blockFile.innerHTML = '';
+}
+
 avatarChooser.addEventListener('change', () => {
-  setImage(avatarChooser, previewAvatar)
+  setImage(avatarChooser, previewAvatar);
 });
 
 fileChooser.addEventListener('change', () => {
-  setImage(fileChooser, previewFile)
+  setImage(fileChooser, getPreviewImage());
 });
+
+export {removeImage, removeAvatar}
