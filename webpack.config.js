9c8bc09a59cd958c70a28108d040c1d8df065bd4
Sergey <szaytsev060576@gmail.com>
2021-09-21 21:50:48 +0300

    добавляет пояснения в конфиг

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const path = require('path');
+const path = require('path'); //путь до корня проекта(реализ через resolve)
 
 module.exports = {
-  entry: './source/js/main.js',
-  devtool: 'source-map',
-  output: {
+  entry: './source/js/main.js', //точка входа
+  devtool: 'source-map',//карта исходников
+  output: {// точка выхода
     filename: 'main.bundle.js',
-    path: path.resolve(__dirname, 'docs/js'),
+    path: path.resolve(__dirname, 'docs/js'),//куда складывается файлы из текущей директории
   },
 };
